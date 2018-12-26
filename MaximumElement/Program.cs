3b313590a523e01eb31c5871b3e9d48d09b4dff0
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-12-26 13:09:55 -0600

    finished

--- MaximumElement/Program.cs
+++ MaximumElement/Program.cs
@@ -1,12 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace MaximumElement
+// class Input
+// {
+//     public long Type { get; set; }
+//     public long Value { get; set; }
+// }
+
+class Solution
 {
-    class Program
+    static void Main(String[] args)
     {
-        static void Main(string[] args)
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        // SOLUTION 1: (this will timeout after test case 17)
+        //         var N = Convert.ToInt64(Console.ReadLine());
+        //         var inputs = new List<Input>();
+        //         var stack = new List<Input>();
+        //         var value = 0L;
+
+        //         for (var i = 0; i < N; i++)
+        //         {
+        //             var line = Console.ReadLine();
+        //             if (line[0] == '1')
+        //             {
+        //                 value = Convert.ToInt64(line.Substring(2));
+        //                 // Console.WriteLine(value);
+        //                 inputs.Add(new Input() { Type = 1, Value = value });
+        //             }
+        //             else if (line[0] == '2')
+        //             {
+        //                 inputs.Add(new Input() { Type = 2, Value = 0 });
+        //             }
+        //             else if (line[0] == '3')
+        //             {
+        //                 inputs.Add(new Input() { Type = 3, Value = 0 });
+        //             }
+        //         }
+
+        //         // foreach (var input in inputs)
+        //         // {
+        //         //     Console.WriteLine(input.type);
+        //         // }
+
+        //         foreach (var input in inputs)
+        //         {
+        //             if (input.Type == 1)
+        //             {
+        //                 stack.Add(input);
+        //             }
+        //             else if (input.Type == 2)
+        //             {
+        //                 stack.RemoveAt(stack.Count - 1);
+        //             }
+        //             else if (input.Type == 3)
+        //             {
+        //                 // Console.WriteLine(stack.Count);
+        //                 Console.WriteLine(stack.Max(i => i.Value));
+        //             }
+        //         }
+
+        // SOLUTION 2: (still getting timeout error after case 17.)
+        var N = Convert.ToInt64(Console.ReadLine());
+        var stack = new List<long>();
+
+        for (var i = 0; i < N; i++)
         {
-            Console.WriteLine("Hello World!");
+            var line = Console.ReadLine();
+            if (line[0] == '1')
+            {
+                var value = Convert.ToInt64(line.Substring(2));
+                stack.Add(value);
+            }
+            else if (line[0] == '2')
+            {
+                stack.RemoveAt(stack.Count - 1);
+            }
+            else if (line[0] == '3')
+            {
+                Console.WriteLine(stack.Max(val => val));
+            }
         }
     }
 }
